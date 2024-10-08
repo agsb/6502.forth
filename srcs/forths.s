fe7e2105f1ae502e53cf174f337ae7afb3d778cd
agsb <barcellos.alvaro@gmail.com>
2024-10-08 15:57:56 -0300

    update code

--- srcs/forths.s
+++ srcs/forths.s
@@ -56,28 +56,84 @@
 .p02
 
 ;---------------------------------------------------------------------
-;    constants
-FALSE = 0
-TRUE = 1
-
-STACKSIZE = $30
-
-HALFSTK =   $30 / 2
-
-TERMINAL  = $100
-
-;---------------------------------------------------------------------
-;   using absolute memory indirect references
+;   A Forth for 6502
+;   using minimal thread code and
+;   absolute memory indirect references
+;   stacks LSB and MSB splited by STACKSIZE
+;
+;   no real SP@ or RP@ or SP! or RP! 
+;   just internal offsets SP0+idx or RP0+idx
 ;
 ;   .byte idx
 ;   .byte idy
-;   .word spt
-;   .word rpt
+
+;   define .word sp0
+;   define .word rp0
+;
 ;   .word ipt
 ;   .word dpt
-;   .word tos
 ;   .word usr
+;   .word tos
+;   .word nos
 ;
+;---------------------------------------------------------------------
+; macros for dictionary, makes:
+;
+;   h_name:
+;   .word  link_to_previous_entry
+;   .byte  strlen(name) + flags
+;   .byte  name
+;   name:
+;
+; label for primitives
+.macro makelabel arg1, arg2
+.ident (.concat (arg1, arg2)):
+.endmacro
+
+; header for primitives
+; the entry point for dictionary is h_~name~
+; the entry point for code is ~name~
+.macro def_word name, label, flag
+makelabel "h_", label
+.ident(.sprintf("H%04X", hcount + 1)) = *
+.word .ident (.sprintf ("H%04X", hcount))
+hcount .set hcount + 1
+.byte .strlen(name) + flag + 0 ; nice trick !
+.byte name
+makelabel "", label
+.endmacro
+
+;---------------------------------------------------------------------
+; variables for macros
+
+hcount .set 0
+
+H0000 = 0
+
+;---------------------------------------------------------------------
+;    constants
+
+FALSE = 0
+
+TRUE = -1
+
+; forth stack size
+STACKSIZE = $24
+
+sps = STACKSIZE
+
+; forth terminal input area
+TERMINAL  = $50
+
+; cell size, two bytes, 16-bit
+CELL = 2
+
+; highlander, immediate flag.
+FLAG_IMM = 1<<7
+
+; length of words
+LENGTH = 15
+
 ;---------------------------------------------------------------------
 ; Forth like functions
 ; to keep code safe by not using "fall throught".
@@ -90,14 +146,21 @@ TERMINAL  = $100
 
 * = $F0
 
-; forth sp index
+; save x
+svx:    .byte $0
+; save y
+svy:    .byte $0
+
+; forth sp index offset
 six:    .byte $0
 ; forth rp index
 rix:    .byte $0
+
 ; data stack base pointer
-spt:    .word $0
+; spt:    .word $0
 ; return stack base pointer
-rpt:    .word $0
+; rpt:    .word $0
+
 ; dictionary next free cell pointer
 dpt:    .word $0
 ; instruction pointer
@@ -108,7 +171,7 @@ ipt:    .word $0
 wrk:    .word $0
 tos:    .word $0
 nos:    .word $0
-; tmp:    .word $0
+tmp:    .word $0
 
 ;---------------------------------------------------------------------
 .segment "CODE"
@@ -118,11 +181,11 @@ tib:
 
 ; could be at page zero, less code, less cycles
 
-.res STACKSIZE, $0
-dat_zero: .word $0
+.res sps, $0
+sp0: .word $0
 
-.res STACKSIZE, $0
-ret_zero: .word $0
+.res sps, $0
+rp0: .word $0
 
 ;---------------------------------------------------------------------
 .segment "ONCE"
@@ -130,256 +193,299 @@ ret_zero: .word $0
 ;---------------------------------------------------------------------
 ;   data stack stuff
 
-keep_: ; to push
-    ; ldx six
-    dex
-    dex
-    stx six
-    rts
-
-lose_: ; to pull
-    ; ldx six
-    inx
-    inx
-    stx six
-    rts
-
 spush:
 push_:
     ldx six
     lda tos + 0
-    sta dat_zero - 2, x
+    sta sp0 - 1, x
     lda tos + 1
-    sta dat_zero - 1, x
+    sta sp0 - 1 + sps, x
     jmp keep_
 
 spull:
 pull_:
     ldx six
-    lda dat_zero + 0, x
+    lda sp0 + 0, x
     sta tos + 0
-    lda dat_zero + 1, x
+    lda sp0 + 0 + sps, x
     sta tos + 1
     jmp lose_
 
+spush2:
 push2_:
     ldx six
     lda nos + 0
-    sta dat_zero - 4, x
+    sta sp0 - 2, x
     lda nos + 1
-    sta dat_zero - 3, x
+    sta sp0 - 2 + sps, x
     lda tos + 0
-    sta dat_zero - 2, x
+    sta sp0 - 1, x
     lda tos + 1
-    sta dat_zero - 1, x
-    jsr keep_
+    sta sp0 - 1 + sps, x
+    dex
     jmp keep_
 
+spull2:
 pull2_:
     ldx six
-    lda dat_zero + 0, x
+    lda sp0 + 0, x
     sta tos + 0
-    lda dat_zero + 1, x
+    lda sp0 + 0 + sps, x
     sta tos + 1
-    lda dat_zero + 2, x
+    lda sp0 + 1, x
     sta nos + 0
-    lda dat_zero + 3, x
+    lda sp0 + 1 + sps, x
     sta nos + 1
-    jsr lose_
+    inx 
     jmp lose_
 
+;----------------------------------------------------------------------
+; ( w -- )
+def_word "drop", "drop", 0
 drop_:
     ldx six
-    jsr lose_
-    ; rts
+lose_:
+    inx
+    stx six
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w -- w w ) 
+def_word "dup", "dup", 0
 dup_:
     ldx six
-    lda dat_zero + 0, x
-    sta dat_zero - 2
-    lda dat_zero + 1, x
-    sta dat_zero - 1
-    jsr keep_
-    ; rts
+    lda sp0 + 0, x
+    sta sp0 - 1, x
+    lda sp0 + 0 + sps, x
+    sta sp0 - 1 + sps, x
+keep_:
+    dex
+    stx six
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- w2 w1 w2) 
+def_word "over", "over", 0
 over_:
     ldx six
-    lda dat_zero + 2, x
-    sta dat_zero - 2
-    lda dat_zero + 3, x
-    sta dat_zero - 1
-    jsr keep_
-    ; rts
-    jmp link_
+    lda sp0 + 1, x
+    sta sp0 - 1, x
+    lda sp0 + 1 + sps, x
+    sta sp0 - 1 + sps, x
+    jmp keep_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- w2 w1 ) 
+def_word "swap", "swap", 0
 swap_:
     ldx six
-    lda dat_zero + 0, x
-    sta dat_zero - 2
-    lda dat_zero + 1, x
-    sta dat_zero - 1
-    lda dat_zero + 2, x
-    sta dat_zero + 0
-    lda dat_zero + 3, x
-    sta dat_zero + 1
-    lda dat_zero - 2, x
-    sta dat_zero + 2
-    lda dat_zero - 1, x
-    sta dat_zero + 3
-    ; rts
+    lda sp0 + 0, x
+    sta sp0 - 1, x
+    lda sp0 + 0 + sps, x
+    sta sp0 - 1 + sps, x
+    lda sp0 + 1, x
+    sta sp0 + 0, x
+    lda sp0 + 1 + sps, x
+    sta sp0 + 0 + sps, x
+    lda sp0 - 1, x
+    sta sp0 + 1
+    lda sp0 - 1 + sps, x
+    sta sp0 + 1 + sps, x
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 w2 w3 -- w3 w1 w2 ) 
+def_word "rot", "rot", 0
 rot_:
     ldx six
-    lda dat_zero + 4, x
-    sta dat_zero - 2
-    lda dat_zero + 5, x
-    sta dat_zero - 1
-    lda dat_zero + 2, x
-    sta dat_zero + 4
-    lda dat_zero + 3, x
-    sta dat_zero + 5
-    lda dat_zero + 0, x
-    sta dat_zero + 2
-    lda dat_zero + 1, x
-    sta dat_zero + 3
-    lda dat_zero - 2, x
-    sta dat_zero + 0
-    lda dat_zero - 1, x
-    sta dat_zero + 1
-    ; rts
+    lda sp0 + 2, x
+    sta sp0 - 1, x
+    lda sp0 + 2 + sps, x
+    sta sp0 - 1 + sps, x
+    lda sp0 + 1, x
+    sta sp0 + 2, x
+    lda sp0 + 1 + sps, x
+    sta sp0 + 2 + sps, x
+    lda sp0 + 0, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    sta sp0 + 1 + sps
+    lda sp0 - 1, x
+    sta sp0 + 0, x
+    lda sp0 - 1 + sps, x
+    sta sp0 + 0 + sps, x
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 AND w2) ) 
+def_word "and", "and", 0
 and_:
     ldx six
-    lda dat_zero + 0, x
-    and dat_zero + 2, x
-    sta dat_zero + 2, x
-    lda dat_zero + 1, x
-    and dat_zero + 3, x
-    sta dat_zero + 3, x
-    jmp drop_
+    lda sp0 + 0, x
+    and sp0 + 1, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    and sp0 + 1 + sps, x
+    sta sp0 + 1 + sps, x
+    jmp lose_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 OR w2) ) 
+def_word "or", "or", 0
 or_:
     ldx six
-    lda dat_zero + 0, x
-    ora dat_zero + 2, x
-    sta dat_zero + 2, x
-    lda dat_zero + 1, x
-    ora dat_zero + 3, x
-    sta dat_zero + 3, x
-    jmp drop_
+    lda sp0 + 0, x
+    ora sp0 + 1, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    ora sp0 + 1 + sps, x
+    sta sp0 + 1 + sps, x
+    jmp lose_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 XOR w2) ) 
+def_word "xor", "xor", 0
 xor_:
     ldx six
-    lda dat_zero + 0, x
-    eor dat_zero + 2, x
-    sta dat_zero + 2, x
-    lda dat_zero + 1, x
-    eor dat_zero + 3, x
-    sta dat_zero + 3, x
-    jmp drop_
+    lda sp0 + 0, x
+    eor sp0 + 1, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    eor sp0 + 1 + sps, x
+    sta sp0 + 1 + sps, x
+    jmp lose_
 
+;----------------------------------------------------------------------
 cpt_:
     ldx six
     sec
-    tya
-    sbc dat_zero + 0, x
-    sta dat_zero + 0, x
+    pha
+    sbc sp0 + 0, x
+    sta sp0 + 0, x
     sec
-    tya
-    sbc dat_zero + 1, x
-    sta dat_zero + 1, x
+    pla
+    sbc sp0 + 0 + sps, x
+    sta sp0 + 0 + sps, x
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 -- ($0000 - w1) ) 
+def_word "neg", "negate", 0
 neg_:
     lda #$00
-    tay
     jmp cpt_
 
+;----------------------------------------------------------------------
+; ( w1 -- ($FFFF - w1) ) 
+def_word "inv", "invert", 0
 inv_:
     lda #$FF
-    tay
     jmp cpt_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 - w2) ) 
+def_word "-", "sub", 0
 sub_:
     ldx six
     sec
-    lda dat_zero + 2, x
-    sbc dat_zero + 0, x
-    sta dat_zero + 2, x
-    lda dat_zero + 3, x
-    sbc dat_zero + 1, x
-    sta dat_zero + 3, x
+    lda sp0 + 0, x
+    sbc sp0 + 1, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    sbc sp0 + 1 + sps, x
+    sta sp0 + 1 + sps, x
+    clc ; keep carry clear
     jmp drop_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 + w2) ) 
+def_word "+", "add", 0
 add_:
     ldx six
     clc
-    lda dat_zero + 2, x
-    adc dat_zero + 0, x
-    sta dat_zero + 2, x
-    lda dat_zero + 3, x
-    adc dat_zero + 1, x
-    sta dat_zero + 3, x
+    lda sp0 + 0, x
+    adc sp0 + 1, x
+    sta sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    adc sp0 + 1 + sps, x
+    sta sp0 + 1 + sps, x
     jmp drop_
 
+;----------------------------------------------------------------------
 cmp_:
     ldx six
     sec
-    lda dat_zero + 2, x
-    sbc dat_zero + 0, x
-    lda dat_zero + 3, x
-    sbc dat_zero + 1, x
+    lda sp0 + 0, x
+    sbc sp0 + 1, x
+    lda sp0 + 0 + sps, x
+    sbc sp0 + 1 + sps, x
     rts
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 = w2) ) 
+def_word "=", "eq", 0
 eq_:
     jsr cmp_
     beq true2_
     bne false2_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 < w2) ) 
+def_word "<", "lt", 0
 lt_:
     jsr cmp_
     bmi true2_
     bpl false2_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- (w1 > w2) ) 
+def_word ">", "gt", 0
 gt_:
     jsr cmp_
-    bmi false2_
-    beq false2_
     bpl true2_
+    ; bmi false2_
+    ; beq false2_
 
-same2_:
-    ldx six
-    sta dat_zero + 2, x
-    sta dat_zero + 3, x
-    jmp drop_
-
+;----------------------------------------------------------------------
 false2_:
     lda #(FALSE)
     beq same2_
 
+;----------------------------------------------------------------------
 true2_:
     lda #(TRUE)
-    bne same2_
+    bne same2_   ; could be falltrought
 
+;----------------------------------------------------------------------
+same2_:
+    ldx six
+    sta sp0 + 2, x
+    sta sp0 + 2, x
+    inx 
+    jmp lose_
+
+;----------------------------------------------------------------------
+; ( w1 -- (w1 << 1) ) 
+def_word "shl", "shl", 0
 shl_:
     ldx six
-    asl dat_zero + 0, x
-    rol dat_zero + 1, x
+    asl sp0 + 0, x
+    rol sp0 + 0 + sps, x
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 -- (w1 >> 1) ) 
+def_word "shr", "shr", 0
 shr_:
     ldx six
-    lsr dat_zero + 0, x
-    ror dat_zero + 1, x
+    lsr sp0 + 0, x
+    ror sp0 + 0 + sps, x
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
 cto_:
     jsr pull2_
     ldy #0
@@ -394,72 +500,102 @@ to_:
     sta (tos), y
     rts
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- ) *w1 = (0x00FF AND w2)
+def_word "cstore", "c!", 0
 cStore_:
     jsr cto_
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- ) *w1 = w2 
+def_word "store", "!", 0
 store_:
     jsr to_
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1  -- w2 ) w2 = 0x00FF AND @w1 
+def_word "cat", "c@", 0
 cat_:
     ldx six
-    lda dat_zero + 0, x
+    lda sp0 + 0, x
     sta tos + 0
-    lda dat_zero + 1, x
+    lda sp0 + 0 + sps, x
     sta tos + 1
+    tya
+    pha
     ldy #0
-    lda (tos), y
-    sta dat_zero + 0, x
+    sta sp0 + 0 + sps, x
+    lda (tos, x)
+    sta sp0 + 0, x
     rts
 
 at_:
     jsr cat_
     iny
     lda (tos), y
-    sta dat_zero + 1, x
+    sta sp0 + 1, x
     rts
 
+;----------------------------------------------------------------------
+; ( w1  -- w2 ) w2 = 0x00FF AND *w1 
+def_word "cfetch", "c@", 0
 cFetch_:
     jsr cat_
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1  -- w2 ) w2 = *w1 
+def_word "fetch", "@", 0
 fetch_:
     jsr at_
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1  -- w2 ) w2 = *w1 
+def_word "incr", "1+", 0
 incr_:
     ldx six
-    inc dat_zero + 0, x
+    inc sp0 + 0, x
     bne @ends
-    inc dat_zero + 1, x
+    inc sp0 + 0 + sps, x
 @ends:
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1  -- w2 ) w2 = *w1 
+def_word "decr", "1-", 0
 decr_:
     ldx six
-    lda dat_zero + 0, x
+    lda sp0 + 0, x
     bne @ends
-    dec dat_zero + 1, x
+    dec sp0 + 0 + sps, x
 @ends:
-    dec dat_zero + 0, x
+    dec sp0 + 0, x
     ; rts
     jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 -- )  
+def_word "exec", "exec", 0
 goto_:
     ldx six
-    lda dat_zero + 1,x
+    lda sp0 + 0 + sps, x
     pha
-    lda dat_zero + 0,x
+    lda sp0 + 0, x
     pha
     php
     rti
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- )  
+def_word "addto", "+!", 0
 addto_:
     jsr pull2_
     ldy #0
@@ -474,6 +610,9 @@ addto_:
     ; rts
 	jmp link_
 
+;----------------------------------------------------------------------
+; ( w1 w2 -- )  
+def_word "subto", "-!", 0
 subto_:
     jsr pull2_
     ldy #0
@@ -495,9 +634,9 @@ rpush:
 rpush_:
     ldx rix
     lda tos + 0
-    sta ret_zero - 2, x
+    sta rp0 - 1, x
     lda tos + 1
-    sta ret_zero - 1, x
+    sta rp0 - 1 + sps, x
     dex
     dex
     stx rix
@@ -506,9 +645,9 @@ rpush_:
 rpull:
 rpull_:
     ldx rix
-    lda ret_zero + 0, x
+    lda rp0 + 0, x
     sta tos + 0
-    lda ret_zero + 1, x
+    lda rp0 + 0 + sps, x
     sta tos + 1
     inx
     inx
@@ -519,9 +658,9 @@ rpull_:
 
 rshow_:
     ldx rix
-    lda ret_zero + 0, x
+    lda rp0 + 0, x
     sta tos + 0
-    lda ret_zero + 1, x
+    lda rp0 + 1, x
     sta tos + 1
     jsr push_
     ; rts
@@ -576,14 +715,14 @@ t2ret_:
 opin:
     ldx six
     ; pseudo tos
-    lda dat_zero + 0, x
+    lda sp0 + 0, x
     sta wrk + 0
-    lda dat_zero + 1, x
+    lda sp0 + 1, x
     sta wrk + 1
     ; pseudo nos
-    lda dat_zero + 2, x
+    lda sp0 + 2, x
     sta tmp + 0
-    lda dat_zero + 3, x
+    lda sp0 + 3, x
     sta tmp + 1
     ; clear results
     lda #0
@@ -601,13 +740,13 @@ opout:
     ; copy results
     ldx six
     lda nos + 0
-    sta dat_zero + 0, x
+    sta sp0 + 0, x
     lda nos + 1
-    sta dat_zero + 1, x
+    sta sp0 + 1, x
     lda tos + 0
-    sta dat_zero + 2, x
+    sta sp0 + 2, x
     lda tos + 1
-    sta dat_zero + 3, x
+    sta sp0 + 3, x
     ; rts
     jmp link_
 
@@ -691,9 +830,9 @@ mul_:
 unnest_:  ; aka semis:
     ; pull from return stack
     ldx rix
-    lda ret_zero + 0, x
+    lda rp0 + 0, x
     sta ipt + 0
-    lda ret_zero + 1, x
+    lda rp0 + 0 + sps, x
     sta ipt + 1
     inx
     inx
@@ -729,9 +868,9 @@ nest_:
     ; push into return stack
     ldx rix
     lda ipt + 0
-    sta ret_zero - 2, x
+    sta rp0 - 1, x
     lda ipt + 1
-    sta ret_zero - 1, x
+    sta rp0 - 1 + sps, x
     dex
     dex
     stx rix
@@ -748,6 +887,7 @@ jump_:
     ; do the jump
     jmp (wrk)
 
+;----------------------------------------------------------------------
 ;----------------------------------------------------------------------
 ; extras for 6502
 ; vide eorBookV1.0.1
